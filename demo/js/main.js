536729cbfef1de2ffde0f7cc1a27b904853729fe
Brian Lam <brianlam@stanford.edu>
2014-05-19 16:47:44 -0700

    changed demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,69 +1,9 @@
-firebaseRef = new Firebase("https://dais.firebaseio.com/demo/presentation");
+firebaseRef = new Firebase("https://dais.firebaseio.com/demo/");
 
-// viewpoints = [1,2,3,4,5,6,7,8,9]; 
-// presentation = {left_coor: -80, center_coor: 0, right_coor: 80, viewpoints: viewpoints}
-// firebaseRef.set(presentation); 
-
-firebaseRef.on("value", function(snapshot) {
-  presentation = snapshot.val();
-  console.log(presentation);
-
-  // sort orientations
-  presentation.orientations.sort();
-
-
-  // NUM_SEGMENTS
-  NUM_SEGMENTS = 25;
-
-  // countOrientationsBySegment()
-  segmentCounters = [];
-  segmentProportions = [];
-  numOrientations = presentation.orientations.length;
-  range = presentation.mRightHeading - presentation.mLeftHeading;
-
-  nextBreakpoint = presentation.mLeftHeading;
-  nextBreakpointIndex = 1;
-  currCounter = 0;
-  totalCounter = 0;
-  for (var i = 0; i < numOrientations; i++) {
-    if (presentation.orientations[i] > nextBreakpoint) {
-      segmentCounters.push(currCounter);
-      currCounter = 0;
-      nextBreakpointIndex++;
-      nextBreakpoint = presentation.mLeftHeading + nextBreakpointIndex.toFixed(2)*(range / NUM_SEGMENTS.toFixed(2));
-      // console.log("Next Breakpoint: " + nextBreakpoint);
-    } else {
-      currCounter++;
-    }
-    totalCounter++;
-  }
-
-  for (var i = 0; i < NUM_SEGMENTS; i++) {
-    if (i < segmentCounters.length) {
-      segmentProportions.push(segmentCounters[i].toFixed(2)/totalCounter);
-      console.log("segment proportion: " + segmentProportions[i]);
-    } else {
-      segmentProportions.push(0.0);
-    }
-  }
-
-  for (var i = 0; i < NUM_SEGMENTS; i++) {
-    // colorHeatmapSegment
-    var redColor = segmentProportions[i] * 1000;
-    console.log("segment: " + i + " color: " + redColor);
-    document.getElementById(i).style.background = "rgb(" + redColor.toFixed(0) + ", 0,0)";
-  }
-
-
-
-}); 
-
-
-function createDivs() {
-  var string = "";
-  for (var i = 0; i < 25; i++) {
-    string +='<div class="example_div" id="' + i + '"></div>';
-  }
-  return string;
-}
-
+presentations = [];
+firebaseRef.on("child_added", function(snapshot) {
+    // new presentation added
+    newPresentation = new Presentation(snapshot.val());
+    presentations.push(newPresentation);
+    document.body.appendChild(presentations[presentations.length - 1].displayHeatMap());
+  });
