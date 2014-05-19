b6b5be1fe9da46a8326c87a549a4bef831e680ae
unknown <hwray@stanford.edu>
2014-05-19 14:17:16 -0700

    Added Look Left and Look Right feedback to PrepPresentationActivity, based on how long the user has consecutively been looking at the left or right side of the room.

--- src/com/stanford/dais/Globals.java
+++ src/com/stanford/dais/Globals.java
@@ -10,6 +10,13 @@ public class Globals extends Application {
 	
 	public float mHeadingLeft = 0; 
 	public float mHeadingRight = 0; 
+	public float mHeadingCenter = 0; 
+		
+	public float mTimeLeft = 0; 
+	public float mTimeRight = 0; 
+	public float mGazeTime = 0; 
+
+	public int mGazeSide = 0; 
 	
 	public ArrayList<Float> orientations = new ArrayList<Float>(); 
 		
@@ -20,7 +27,11 @@ public class Globals extends Application {
 	
 	public void clearGlobals() {
 		mHeadingLeft = 0; 
-		mHeadingRight = 0; 
+		mHeadingRight = 0;
+		mHeadingCenter = 0; 
+		mTimeLeft = 0; 
+		mTimeRight = 0; 
+		mGazeTime = 0; 
 		orientations = new ArrayList<Float>(); 
 	}
 	
--- src/com/stanford/dais/PrepPresentationActivity.java
+++ src/com/stanford/dais/PrepPresentationActivity.java
@@ -3,6 +3,8 @@ package com.stanford.dais;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.Header;
+
 import com.google.android.glass.media.Sounds;
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
@@ -37,32 +39,25 @@ public class PrepPresentationActivity extends Activity {
 	private View mMainView; 
 	private TextView mTitleView; 
 	private TextView mHeadingView; 
-	
-    private OrientationManager mOrientationManager;
-    private float mCenterHeading = 0; 
-    
+	private TextView mLeftHeadingView; 
+	private TextView mRightHeadingView; 
+
+    private static final float TOO_LONG_GAZE_TIME = 10.0f; 
     private static final float TOO_STEEP_PITCH_DEGREES = 10.0f;
     
+    private OrientationManager mOrientationManager;
+    private boolean mInterference; 
+    
     private GestureDetector mGestureDetector;
+    
+    private Thread mGazeThread;  
 	
     private final OrientationManager.OnChangedListener mCompassListener =
             new OrientationManager.OnChangedListener() {
 
         @Override
         public void onOrientationChanged(OrientationManager orientationManager) {
-        	if (g.mHeadingLeft == 0 || g.mHeadingRight == 0) {
-            	mHeadingView.setText("" + orientationManager.getHeading());
-        	} else if (orientationManager.getPitch() > TOO_STEEP_PITCH_DEGREES) {
-        		mTitleView.setText("Look up!"); 
-        	} else {
-        		float orientation = orientationManager.getHeading(); 
-        		g.orientations.add(orientation); 
-        		if (orientation > g.mHeadingLeft && orientation < g.mHeadingRight) {
-        			mTitleView.setText("");
-        		} else {
-        			mTitleView.setText("Blowing it"); 
-        		}
-        	}
+        	// Do nothing
         }
 
         @Override
@@ -72,8 +67,7 @@ public class PrepPresentationActivity extends Activity {
 
         @Override
         public void onAccuracyChanged(OrientationManager orientationManager) {
-            //mInterference = orientationManager.hasInterference();
-            //updateTipsView();
+            mInterference = orientationManager.hasInterference();
         }
     };
     
@@ -84,13 +78,14 @@ public class PrepPresentationActivity extends Activity {
         
         this.g = (Globals) getApplication(); 
         
-        g.mHeadingLeft = 0; 
-        g.mHeadingRight = 0; 
-        g.orientations = new ArrayList<Float>(); 
+        g.clearGlobals(); 
+        mInterference = false; 
         
         mMainView = (View) findViewById(R.id.prep_presentation_container); 
         mHeadingView = (TextView) findViewById(R.id.compass_heading); 
         mTitleView = (TextView) findViewById(R.id.instructions_and_feedback); 
+        mLeftHeadingView = (TextView) findViewById(R.id.left_heading); 
+        mRightHeadingView = (TextView) findViewById(R.id.right_heading); 
         
         mGestureDetector = createGestureDetector(this); 
         
@@ -103,6 +98,8 @@ public class PrepPresentationActivity extends Activity {
         
         mOrientationManager.addOnChangedListener(mCompassListener);
         mOrientationManager.start();
+        
+        initGazeThread(); 
     }
     
     
@@ -116,13 +113,10 @@ public class PrepPresentationActivity extends Activity {
                         // mAudioManager.playSoundEffect(Sounds.TAP);
                     	if (g.mHeadingLeft == 0) {
                     		g.mHeadingLeft = mOrientationManager.getHeading(); 
-                    		TextView leftHeadingView = (TextView) mMainView.findViewById(R.id.left_heading); 
-                    		leftHeadingView.setText("" + g.mHeadingLeft); 
+                    		mLeftHeadingView.setText("" + g.mHeadingLeft); 
                     		mTitleView.setText("Look at right side of room and tap"); 
                     	} else if (g.mHeadingRight == 0) {
                     		g.mHeadingRight = mOrientationManager.getHeading(); 
-                    		TextView rightHeadingView = (TextView) mMainView.findViewById(R.id.right_heading); 
-                    		rightHeadingView.setText("" + g.mHeadingRight); 
                     		
                     		if (g.mHeadingRight < g.mHeadingLeft) {
                     			float temp = g.mHeadingRight; 
@@ -130,10 +124,12 @@ public class PrepPresentationActivity extends Activity {
                     			g.mHeadingLeft = temp; 
                     		}
                     		
-                    		mCenterHeading = (g.mHeadingLeft + g.mHeadingRight) / 2; 
+                    		g.mHeadingCenter = (g.mHeadingLeft + g.mHeadingRight) / 2; 
                     		
+                    		mLeftHeadingView.setText("");
+                    		mRightHeadingView.setText("");
                     		mHeadingView.setText(""); 
-                    	}
+                  	}
                         return true;
                     } else if (gesture == Gesture.TWO_TAP) {
                         // do something on two finger tap
@@ -162,5 +158,68 @@ public class PrepPresentationActivity extends Activity {
         return false;
     }
 
-	
+    private void initGazeThread() {
+		mGazeThread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						if (mInterference) {
+							mHeadingView.setText("Magnetic interference");
+							continue; 
+						}
+						
+			        	if (g.mHeadingLeft == 0 || g.mHeadingRight == 0) {
+			            	mHeadingView.setText("" + mOrientationManager.getHeading());
+			        	}
+			        	
+			        	if (mOrientationManager.getPitch() > TOO_STEEP_PITCH_DEGREES) {
+			        		mTitleView.setText("Look up!"); 
+			        	} else {
+			        		float orientation = mOrientationManager.getHeading(); 
+			        		g.orientations.add(orientation); 
+			        		
+			        		if (orientation > g.mHeadingLeft && orientation < g.mHeadingCenter) {
+			        			if (g.mGazeSide != 0) {
+				        			g.mGazeSide = 0; 
+			        				g.mGazeTime = 0; 
+			        			}
+			        			g.mGazeTime += 1; 
+			        			
+			        			if (g.mGazeTime > TOO_LONG_GAZE_TIME) {
+			        				mTitleView.setText("Look right!"); 
+			        			} else {
+				        			mTitleView.setText("");
+			        			}
+			        			
+			        			g.mTimeLeft += 1; 
+			        		} 
+			        		else if (orientation > g.mHeadingCenter && orientation < g.mHeadingRight) {
+			        			if (g.mGazeSide != 1) {
+				        			g.mGazeSide = 1; 
+				        			g.mGazeTime = 0; 
+			        			}
+			        			g.mGazeTime += 1; 
+			        			
+			        			if (g.mGazeTime < TOO_LONG_GAZE_TIME) {
+			        				mTitleView.setText("Look left!");
+			        			} else {
+			        				mTitleView.setText(""); 
+			        			}
+			        			
+			        			g.mTimeRight += 1; 
+			        		} 
+			        		else {
+			        			mTitleView.setText("Blowing it"); 
+			        		}
+			        	}
+						
+						Thread.sleep(100);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		mGazeThread.start(); 
+    }
 }
